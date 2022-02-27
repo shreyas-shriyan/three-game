5b6ce02b2d0d8b0cf936b9976b6ac81feca2f5c6
shreyas-s <shreyas.s@dror.co.in>
2022-02-27 22:49:18 +0530

    added guesture detection

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import Home from "./pages/home";
 
 function App() {
-
   return (
     <div className="App">
-      <Home/>
+      <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,28 @@
-const styles = {
-  home: "h-screen border-2 text-center align-middle",
-};
+import { useDrag } from "@use-gesture/react";
+import { useState } from "react";
 
 const Home = () => {
-  return <div className={styles.home}>Home</div>;
+  const [swipe, setSwipe] = useState("");
+  const swipeHandler = useDrag((state) => {
+    const [xAxis, yAxis] = state.swipe;
+
+    // detect swipe direction
+    if (xAxis !== 0) {
+      xAxis === 1 ? setSwipe("r") : setSwipe("l");
+    } else if (yAxis !== 0) {
+      yAxis === 1 ? setSwipe("d") : setSwipe("u");
+    }
+  });
+
+  return (
+    <div className={styles.home} {...swipeHandler()}>
+      <div className="m-auto">{swipe}</div>
+    </div>
+  );
+};
+
+const styles = {
+  home: "h-screen border-2 text-center align-middle touch-none",
 };
 
 export default Home;
